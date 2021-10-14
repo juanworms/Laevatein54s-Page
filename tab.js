2106dc30a22ac839729b8818e39686a8926b097a
Juan <juan.w.benitez@gmail.com>
2021-10-13 22:03:39 -0300

    added RDC tah in Videogame section

--- tab.js
+++ tab.js
@@ -2,9 +2,11 @@ function mostrarPestaña (pesta)
 {
 	div1 = document.getElementById('cftw');
     div2 = document.getElementById('Super42');
-    
+    div3 = document.getElementById('RDC');
+
     div1.style.display = 'none';
     div2.style.display = 'none';
+    div3.style.display = 'none';
     
     divSeleccionado = document.getElementById(pesta);
     divSeleccionado.style.display = 'block';
